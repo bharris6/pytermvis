2e2d0a701ddcb0b602cf0c3e8d155c05b8ce14c8
Brian Harrison <harrison.brian.d@gmail.com>
2020-04-07 17:36:39 -0400

    Added some more docstrings to asciimatics_vis.py

--- asciimatics_vis.py
+++ asciimatics_vis.py
@@ -16,9 +16,24 @@ CHARACTER="*"
 
 
 def display_loop(screen, rec, char):
+    """
+    The loop which handles getting an audio sample, computing its
+    frequency transformation, and then displaying the resulting 
+    FFT data visually.
+
+    :param screen: Asciimatics Screen representing the drawing surface
+    :type screen: asciimatics.screen.Screen
+
+    :param rec: SoundCard recording device to get audio samples from
+    
+    :param char: Character to use in the visualization display
+    :type char: string -- one-character
+
+    :raises asciimatics.exceptions.ResizeScreenError: Screen Resizing Event
+    """
     # Since we are using a horizontal graph (bars go vertical)...
-    # term_width is the number of buckets to get
-    # term_height is the maximum amplitude/value for a bucket
+    #  --> term_width is the number of buckets to get
+    #  --> term_height is the maximum amplitude/value for a bucket
     old_values = None
     while True:
         frq, channel_data = sample(rec)
@@ -43,9 +58,9 @@ def display_loop(screen, rec, char):
                 for val in range(bucket*stepsize, (bucket+1)*stepsize):
                     vals.append(channel[val])
                 
-            # Now we want to normalize all those values to between 0 and the maximum we can print 
-            # And then sum them up
-            val_sum = (math.fsum( normalize(0, term_height, vals) )) if len(vals) > 0 else 0
+            # Now we want to normalize all those values to between 0 
+            # and the maximum we can print, and then sum them up
+            val_sum = (math.fsum(normalize(0, term_height, vals))) if len(vals) > 0 else 0
             buckets.append(int(min(term_height, val_sum)))
 
         for x, bheight in enumerate(buckets):
@@ -55,7 +70,8 @@ def display_loop(screen, rec, char):
                     old_height = old_values[x]
                     
                     if old_height < bheight:
-                        # Draw characters back from old height to new height
+                        # Draw characters back from old height to 
+                        # new height
                         screen.move(x, term_height - old_height + 1)
                         screen.draw(x, term_height, char=char)
                     elif old_height > bheight:
@@ -64,10 +80,7 @@ def display_loop(screen, rec, char):
                         screen.move(x, 0)
                         screen.draw(x, term_height - bheight - 1, char=" ")
                 else:
-                    # Draw entire line
-                    #screen.move(x, 0)
-                    #screen.draw(x, term_height - bheight, char=" ")
-                    
+                    # Draw whole line
                     screen.move(x, term_height - bheight)
                     screen.draw(x, term_height, char=char)
                     
@@ -78,7 +91,7 @@ def display_loop(screen, rec, char):
         
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description="Python Terminal Visualizer")
+    parser = argparse.ArgumentParser(description="pytermvis -- Python Terminal Visualizer")
 
     parser.add_argument("-c", "--char", action="store", dest="char", default=CHARACTER)
     parser.add_argument("-s", "--sample-rate", action="store", dest="samplerate", default=SAMPLERATE)
@@ -99,7 +112,6 @@ if __name__ == '__main__':
     with mixin.recorder(samplerate=SAMPLERATE) as rec:
         while True:
             try:
-                #Screen.wrapper(display_loop, arguments=[rec])
                 with ManagedScreen() as screen:
                     display_loop(screen, rec, parser_args.char)
             except ResizeScreenError as e:

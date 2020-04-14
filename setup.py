0611fcf85651201cfb475d57ca5d3671f6209835
Brian Harrison <harrison.brian.d@gmail.com>
2020-04-13 22:41:08 -0400

    Added gitignore, and a setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+        name = "pytermvis",
+        version = "0.2",
+        description = "Python Terminal Audio Visualizer",
+        url = "https://github.com/bharris6/pytermvis",
+        author = "Brian Harrison",
+        author_email = "bharriso@funnybirdbrews.com",
+        license = "MIT",
+        packages=["pytermvis"],
+        zip_safe=False
+    )
